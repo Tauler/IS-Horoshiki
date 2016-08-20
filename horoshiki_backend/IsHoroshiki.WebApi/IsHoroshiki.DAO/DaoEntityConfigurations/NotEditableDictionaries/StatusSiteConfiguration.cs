76811a35ff1c7bd7d7d2800f606d7076bc5af12d
Evgeny Prokhorov <prohoroven83@mail.ru>
2016-08-20 17:30:13 +0300

    https://ishoroshiki.atlassian.net/browse/BACKOFFICE-47

---
+++ horoshiki_backend/IsHoroshiki.WebApi/IsHoroshiki.DAO/DaoEntityConfigurations/NotEditableDictionaries/StatusSiteConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IsHoroshiki.DAO.DaoEntities.NotEditableDictionaries;
+
+namespace IsHoroshiki.DAO.DaoEntityConfigurations.NotEditableDictionaries
+{
+    /// <summary>
+    /// Конфигурация Способы покупки
+    /// </summary>
+    public class StatusSiteConfiguration : BaseNotEditableDictionaryDaoEntityConfiguration<StatusSite>
+    {
+        #region Конструктор
+
+        /// <summary>  
+        /// Конструктор  
+        /// </summary>  
+        public StatusSiteConfiguration() 
+            : base("StatusSites")
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ horoshiki_backend/IsHoroshiki.WebApi/IsHoroshiki.DAO/Repositories/NotEditableDictionaries/StatusSiteRepository.cs
@@ -0,0 +1,24 @@
+﻿using IsHoroshiki.DAO.DaoEntities.NotEditableDictionaries;
+
+namespace IsHoroshiki.DAO.Repositories.NotEditableDictionaries
+{
+    /// <summary>
+    /// Репозиторий Статус площадки
+    /// </summary>
+    public class StatusSiteRepository : BaseNotEditableDictionaryRepository<StatusSite>
+    {
+        #region Конструктор
+
+        /// <summary>  
+        /// Конструктор  
+        /// </summary>  
+        /// <param name="context">Контекст выполнения БД</param>  
+        public StatusSiteRepository(ApplicationDbContext context)
+            : base(context)
+        {
+
+        }
+
+        #endregion
+    }
+}
