e2080cd7e1cdcf01100b91050421927d4eecddb8
onyushkindv <onyushkindv@rvigroup.ru>
2016-08-25 13:15:01 +0300

    https://ishoroshiki.atlassian.net/browse/BCOF-11

--- horoshiki_fronend/angular-app/service/AccountService.js
+++ horoshiki_fronend/angular-app/service/AccountService.js
@@ -10,8 +10,8 @@ accountServices.service('AccountService', ['$http', function($http) {
     this.login = function (username, password) {
 
         var resp = $http({
-            url: "http://192.168.11.4:36791/token",
-            method: "POST",
+            url: backendServerAddr+'/token',
+            method: 'POST',
             data: $.param({ grant_type: 'password', username: username, password: password }),
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         });
