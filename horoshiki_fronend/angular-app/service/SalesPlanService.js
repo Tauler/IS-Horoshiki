7fc46f8df749bc2fc7c66bc17e333f5619036e19
onyushkindv <dmitry.onyushkin@yandex.ru>
2016-10-26 16:18:41 +0300

    исправление интерфейса

---
+++ horoshiki_fronend/angular-app/service/SalesPlanService.js
@@ -0,0 +1,27 @@
+/**
+ * Created by onyushkindv on 24.10.2016.
+ */
+var salesPlanServices = angular.module('salesPlanServices', []);
+
+salesPlanServices.service('SalesPlanService', ['$http', function($http) {
+    this.add = function (object) {
+        var resp = $http({
+            url: backendServerAddr+'/api/salePlan/add',
+            method: 'POST',
+            data: object,
+            headers:  getToken()
+        });
+        return resp;
+    };
+
+    this.edit = function (object) {
+        var resp = $http({
+            url: backendServerAddr+'/api/salePlan/Update',
+            method: 'POST',
+            data: object,
+            headers:  getToken()
+        });
+        return resp;
+    };
+
+}]);
