cf3c6d17b7fe49843a85de5a2511b0ee1908bb91
Alexey Oleynichenko <oalexey@inbox.ru>
2016-11-11 17:55:44 +0300

    https://ishoroshiki.atlassian.net/browse/BCOF-193 Скрипт создания таблицы в БД. Добавление чейнсета в ликвибейз

---
+++ horoshiki_backend/IsHoroshiki.WebApi/IsHoroshiki.DAO/Script/2016-11-11 Цели на месяц.sql
@@ -0,0 +1,23 @@
+CREATE TABLE [dbo].[MonthObjectives](
+	[Id] [int] NOT NULL,
+	[PlatformId] [int] NOT NULL,
+	[Year] [int] NOT NULL,
+	[Month] [int] NOT NULL,
+	[ChecksPerHourForPositionSushiChef] [float] NOT NULL,
+	[ChecksPerHourForPositionCourier] [float] NOT NULL,
+	[ChecksPerHourForPositionPizzaChef] [float] NOT NULL,
+ CONSTRAINT [PK_MonthObjectives] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[MonthObjectives]  WITH CHECK ADD  CONSTRAINT [FK_dbo.MonthObjectives_dbo.Platforms_PlatformId] FOREIGN KEY([PlatformId])
+REFERENCES [dbo].[Platforms] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[MonthObjectives] CHECK CONSTRAINT [FK_dbo.MonthObjectives_dbo.Platforms_PlatformId]
+GO
