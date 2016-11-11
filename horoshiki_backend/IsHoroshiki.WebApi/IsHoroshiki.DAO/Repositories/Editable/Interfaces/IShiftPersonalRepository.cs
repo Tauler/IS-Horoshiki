fe01c6faef257bbef813bd0fac0b6fb059132f81
Alexey Oleynichenko <oalexey@inbox.ru>
2016-11-11 06:31:40 +0400

    https://ishoroshiki.atlassian.net/browse/BCOF-184 Бизнес-логика

--- horoshiki_backend/IsHoroshiki.WebApi/IsHoroshiki.DAO/Repositories/Editable/Interfaces/IShiftPersonalRepository.cs
+++ horoshiki_backend/IsHoroshiki.WebApi/IsHoroshiki.DAO/Repositories/Editable/Interfaces/IShiftPersonalRepository.cs
@@ -7,6 +7,20 @@ namespace IsHoroshiki.DAO.Repositories.Editable.Interfaces
     /// </summary>
     public interface IShiftPersonalRepository : IBaseRepository<ShiftPersonal>
     {
-        
+        /// <summary>
+        /// Поличить смену
+        /// </summary>
+        /// <param name="positionId">Идентификатор должности</param>
+        /// <param name="shiftTypeId">Идентификатор типа смены</param>
+        /// <returns></returns>
+        ShiftPersonal Get(int positionId, int shiftTypeId);
+
+        /// <summary>
+        /// Проверить существование смены
+        /// </summary>
+        /// <param name="positionId">Идентификатор должности</param>
+        /// <param name="shiftTypeId">Идентификатор типа смены</param>
+        /// <returns></returns>
+        bool IsExist(int positionId, int shiftTypeId);
     }
 }
--- horoshiki_backend/IsHoroshiki.WebApi/IsHoroshiki.DAO/Repositories/Editable/ShiftPersonalRepository.cs
+++ horoshiki_backend/IsHoroshiki.WebApi/IsHoroshiki.DAO/Repositories/Editable/ShiftPersonalRepository.cs
@@ -1,5 +1,6 @@
 ﻿using IsHoroshiki.DAO.DaoEntities.Editable;
 using IsHoroshiki.DAO.Repositories.Editable.Interfaces;
+using System;
 using System.Linq;
 
 namespace IsHoroshiki.DAO.Repositories.Editable
@@ -19,6 +20,32 @@ namespace IsHoroshiki.DAO.Repositories.Editable
             : base(context)
         {
 
+        }
+
+        #endregion
+
+        #region IShiftPersonalRepository
+
+        /// <summary>
+        /// Поличить смену
+        /// </summary>
+        /// <param name="positionId">Идентификатор должности</param>
+        /// <param name="shiftTypeId">Идентификатор типа смены</param>
+        /// <returns></returns>
+        public ShiftPersonal Get(int positionId, int shiftTypeId)
+        {
+            return DbSet.FirstOrDefault(sp => sp.PositionId == positionId && sp.ShiftTypeId == shiftTypeId);
+        }
+
+        /// <summary>
+        /// Проверить существование смены
+        /// </summary>
+        /// <param name="positionId">Идентификатор должности</param>
+        /// <param name="shiftTypeId">Идентификатор типа смены</param>
+        /// <returns></returns>
+        public bool IsExist(int positionId, int shiftTypeId)
+        {
+            return DbSet.Any(sp => sp.PositionId == positionId && sp.ShiftTypeId == shiftTypeId);
         }
 
         #endregion
